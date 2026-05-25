df8bb26a50a6f1fb0ebe3935494d25ece1ce95c6
Luciano Rodrigues <lucianoottor@hotmail.com>
2026-05-25 13:21:34 -0300

    monoio -> colocar legacy

--- src/main.rs
+++ src/main.rs
@@ -32,7 +32,7 @@ fn main() {
     let hnsw_model: &'static StaticHNSW = Box::leak(hnsw_model);
     warmup(hnsw_model, 500);
 
-    let mut rt = monoio::RuntimeBuilder::<monoio::IoUringDriver>::new()
+    let mut rt = monoio::RuntimeBuilder::<monoio::LegacyDriver>::new()
         .build()
         .expect("build monoio runtime");
 
